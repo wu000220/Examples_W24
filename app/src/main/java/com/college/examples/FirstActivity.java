b8c9751e708cf7d5eb27fd1d13d1f37e2c47fac3
gourteacher <ouaazs@algonquincollege.com>
2024-03-07 20:17:13 -0500

    Simple Navigation Drawer Example

--- app/src/main/java/com/college/examples/FirstActivity.java
+++ app/src/main/java/com/college/examples/FirstActivity.java
@@ -1,60 +1,64 @@
 package com.college.examples;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-
-import androidx.annotation.NonNull;
+import android.widget.Toast;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import com.google.android.material.navigation.NavigationView;
 
-
-public class FirstActivity extends AppCompatActivity {
+public class FirstActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView( R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
-        toolbar.setTitle(R.string.toolbar_title); // set the Name
         setSupportActionBar(toolbar);
 
-        toolbar.setSubtitle(R.string.toolbar_subtitle); // the second line
+        //For NavigationDrawer:
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
+                drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
-        toolbar.setLogo(R.drawable.mini_opera_icon);
-    }
+        NavigationView navigationView = findViewById(R.id.nav_view);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu items for use in the action bar
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.example_menu, menu);
-        return true;
-    }
+        navigationView.setNavigationItemSelectedListener(item ->
+            {
+                String message = null;
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
+                int item_id = item.getItemId();
 
-        View v = findViewById(R.id.my_rectangle);
+                //what to do when the menu item is selected:
+                if (item_id  == R.id.share_item ) {
+                    message = getString(R.string.share_message);
+                }
+                else if (item_id  == R.id.app_bar_search) {
+                    message = getString(R.string.search_message);
+                }
+                else if ( item_id  == R.id.mail_item) {
+                    message = getString(R.string.email_message);
+                }
+                else if ( item_id == R.id.video_item) {
+                    message = getString(R.string.video_message);
+                }
+                else if ( item_id  == R.id.call_item) {
+                    message = getString(R.string.call_message);
+                }
 
-        if ( id ==  R.id.red_id) {
-            v.setBackgroundColor(Color.RED);
-        }
-        else if (id == R.id.blue_id) {
-            v.setBackgroundColor(Color.BLUE);
-        }
-        else if ( id == R.id.green_id) {
-            v.setBackgroundColor(Color.GREEN);
-        }
-        else if (id == R.id.yellow_id) {
-            v.setBackgroundColor(Color.YELLOW);
-        }
-        return super.onOptionsItemSelected(item);
+                drawerLayout.closeDrawer(GravityCompat.START);
+
+                if ( message != null ) {
+                    Toast.makeText(this,  getString(R.string.navigation_title, message), Toast.LENGTH_LONG).show();
+                }
+                return false ;
+            }
+        );
     }
 }
 
